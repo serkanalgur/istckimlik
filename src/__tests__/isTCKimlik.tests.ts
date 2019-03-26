63a185a0c32f862a72cfbaa75dcf25187cb45c53
Serkan Algur <info@wpadami.com>
2019-03-26 23:00:20 +0400

    📦 Tests

--- src/__tests__/isTCKimlik.tests.ts
+++ src/__tests__/isTCKimlik.tests.ts
@@ -1,18 +1,21 @@
 import { isTCKimlik } from '../index';
+
+// All Turkish National ID Numbers are auto generated for testing
+
 test('TCKimlik Should be True', () => {
-	expect(isTCKimlik(34400221062)).toBe(true);
+	expect(isTCKimlik(34123889266)).toBe(true);
 });
 
 test('TCKimlik Should be False Because Start With zero', () => {
-	expect(isTCKimlik(0o3440022102)).toBe(false);
+	expect(isTCKimlik(0o3440051506)).toBe(false);
 });
 
 test('TCKimlik Should be False Because of Count', () => {
-	expect(isTCKimlik(344002210621)).toBe(false);
+	expect(isTCKimlik(341238892661)).toBe(false);
 });
 
 test('TCKimlik Should be False Because Missing Count', () => {
-	expect(isTCKimlik(3440022106)).toBe(false);
+	expect(isTCKimlik(3412388926)).toBe(false);
 });
 
 test('TCKimlik Should be False Because Fake ID Number', () => {

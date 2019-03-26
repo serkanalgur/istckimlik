871f79766f33d4394be556bf84cf6763444b3cd5
Serkan Algur <info@wpadami.com>
2019-03-26 23:36:44 +0400

    1.0.1

--- src/__tests__/isTCKimlik.tests.ts
+++ src/__tests__/isTCKimlik.tests.ts
@@ -3,21 +3,21 @@ import { isTCKimlik } from '../index';
 // All Turkish Identification Numbers are auto generated for testing
 
 test('TCKimlik Should be True', () => {
-	expect(isTCKimlik(34123889266)).toBe(true);
+  expect(isTCKimlik(34123889266)).toBe(true);
 });
 
 test('TCKimlik Should be False Because Start With zero', () => {
-	expect(isTCKimlik(0o3440051506)).toBe(false);
+  expect(isTCKimlik(0o3440051506)).toBe(false);
 });
 
 test('TCKimlik Should be False Because of Count', () => {
-	expect(isTCKimlik(341238892661)).toBe(false);
+  expect(isTCKimlik(341238892661)).toBe(false);
 });
 
 test('TCKimlik Should be False Because Missing Count', () => {
-	expect(isTCKimlik(3412388926)).toBe(false);
+  expect(isTCKimlik(3412388926)).toBe(false);
 });
 
 test('TCKimlik Should be False Because Fake ID Number', () => {
-	expect(isTCKimlik(48265878921)).toBe(false);
+  expect(isTCKimlik(48265878921)).toBe(false);
 });
--- src/index.ts
+++ src/index.ts
@@ -5,38 +5,38 @@
  */
 
 export function isTCKimlik(TurkishID: number): boolean {
-	// Convert to string for for loops etc.
-	const StringifiedTurkishID = TurkishID.toString();
+  // Convert to string for for loops etc.
+  const StringifiedTurkishID = TurkishID.toString();
 
-	// Check For Start With Zero because Turkish Identification Number cant start with zero
-	const checkIfStartWithZero = 0 !== Number(StringifiedTurkishID.substr(0,1));
+  // Check For Start With Zero because Turkish Identification Number cant start with zero
+  const checkIfStartWithZero = 0 !== Number(StringifiedTurkishID.substr(0, 1));
 
-	// Check For NumberCount of Turkish Identification Number. Count Must Be 11
-	const checkCountEvelen = /^[0-9]{11}$/.test(StringifiedTurkishID);
+  // Check For NumberCount of Turkish Identification Number. Count Must Be 11
+  const checkCountEvelen = /^[0-9]{11}$/.test(StringifiedTurkishID);
 
-	let totalSum: number = 0;
-	let totalPartialSum1: number = 0;
-	let totalPartialSum2: number = 0;
+  let totalSum: number = 0;
+  let totalPartialSum1: number = 0;
+  let totalPartialSum2: number = 0;
 
-	for (let i = 0; i < 10; i++){
-		totalSum += Number(StringifiedTurkishID.substr(i, 1));
-	}
+  for (let i = 0; i < 10; i++) {
+    totalSum += Number(StringifiedTurkishID.substr(i, 1));
+  }
 
-	// Check if mod(10) gives 11th number of Turkish Identification Number
-	const checkModTenForEleven = totalSum % 10 === Number(StringifiedTurkishID.substr(10, 1));
+  // Check if mod(10) gives 11th number of Turkish Identification Number
+  const checkModTenForEleven = totalSum % 10 === Number(StringifiedTurkishID.substr(10, 1));
 
-	for (let i = 0; i < 10; i += 2){
-		totalPartialSum1 += Number(StringifiedTurkishID.substr(i, 1));
-	}
+  for (let i = 0; i < 10; i += 2) {
+    totalPartialSum1 += Number(StringifiedTurkishID.substr(i, 1));
+  }
 
-	for (let i = 1; i < 10; i += 2){
-		totalPartialSum2 += Number(StringifiedTurkishID.substr(i, 1));
-	}
+  for (let i = 1; i < 10; i += 2) {
+    totalPartialSum2 += Number(StringifiedTurkishID.substr(i, 1));
+  }
 
-	// Check if mod(10) of these two variables math gives 10th number of Turkish Identification Number
-	const checkModTenForTen = ((totalPartialSum1 * 7) - totalPartialSum2) % 10 === Number(StringifiedTurkishID.substr(9, 0));
+  // Check if mod(10) of these two variables math gives 10th number of Turkish Identification Number
+  const checkModTenForTen =
+    (totalPartialSum1 * 7 - totalPartialSum2) % 10 === Number(StringifiedTurkishID.substr(9, 0));
 
-
-	// If everything is ok you will get an true for return. If there is a problem, script gives false for every error.
-	return checkIfStartWithZero && checkCountEvelen && checkModTenForEleven && checkModTenForTen;
+  // If everything is ok you will get an true for return. If there is a problem, script gives false for every error.
+  return checkIfStartWithZero && checkCountEvelen && checkModTenForEleven && checkModTenForTen;
 }
